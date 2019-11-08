8191f128f2b05e257370e41bbaf39a646848088b
Lucas Berbesson <lucas.berbesson@fabdev.fr>
2019-11-08 22:23:18 +0100

    designation => get_designation_display

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -5,7 +5,7 @@ from .base import BASE_DIR
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vh4*k+T!-P6RSG&7wv4ZP3ZYFm7qc!bnDj2caKPa+Gw#J5hK=+rrX=reTv5S'  # TODO CHANGE THE WHOLE KEY
+SECRET_KEY = 'THIS_KEY_WILL_BE_CHANGED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -22,38 +22,3 @@ DATABASES = {
 }
 
 WSGI_APPLICATION = 'project.wsgi.application'
-
-FABACCESSLOG_FILE = 'fabaccess.log'
-LOG_DATEFMT = '%Y-%m-%d %H:%M'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'csvformatted': {
-            'format': '{asctime};{levelname};{message}',
-            'datefmt': LOG_DATEFMT,
-            'style': '{'
-        },
-        'simple': {
-            'format': '[{asctime}] [{levelname}] {message}',
-            'datefmt': LOG_DATEFMT,
-            'style': '{'
-        }
-    },
-    'handlers': {
-        'fabaccesslogfile': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': FABACCESSLOG_FILE,
-            'maxBytes': 3 * 1024 * 1024,
-            'backupCount': 3,
-            'formatter': 'csvformatted'
-        },
-    },
-    'loggers': {
-        'fabaccess': {
-            'handlers': ['fabaccesslogfile'],
-            'level': 'DEBUG',
-        },
-    },
-}
