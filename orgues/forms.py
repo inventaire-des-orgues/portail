37904c56a2d8968ae431c66af5d17e4bb318fa91
Lucas Berbesson <lucas.berbesson@fabdev.fr>
2020-02-11 18:47:49 +0100

    Pouvoir éditer le champ "proprietaire"

--- orgues/forms.py
+++ orgues/forms.py
@@ -11,6 +11,7 @@ class OrgueGeneralInfoForm(forms.ModelForm):
             "designation",
             "elevation",
             "etat",
+            "proprietaire",
             "is_polyphone",
             "organisme",
             "lien_reference",
