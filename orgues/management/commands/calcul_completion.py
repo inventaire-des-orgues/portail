65b900ad6a061d1f89bb263870fd7663d807d8b9
Lucas Berbesson <lucas.berbesson@fabdev.fr>
2020-06-23 17:38:12 +0200

    tache manage.py pour recalculer tous les taux de completion

---
+++ orgues/management/commands/calcul_completion.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from orgues.models import Orgue
+
+class Command(BaseCommand):
+    help = "Calcul et enregistrement en base de données le taux d'avancement des fiches"
+
+    def handle(self, *args, **options):
+
+        for orgue in tqdm(Orgue.objects.all()):
+            orgue.completion = orgue.calcul_completion()
+            orgue.save()
