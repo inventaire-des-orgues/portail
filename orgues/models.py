d754f3cca0f154d027c268feced808d6a847734d
gwilherm.poullennec <gwilherm.poullennec@gmail.com>
2020-07-30 22:59:26 +0200

    codification contrainte comme unique

--- orgues/models.py
+++ orgues/models.py
@@ -71,7 +71,7 @@ class Orgue(models.Model):
 
     # Informations générales
     designation = models.CharField(max_length=300, null=True, verbose_name="Désignation", default="orgue", blank=True)
-    codification = models.CharField(max_length=24)
+    codification = models.CharField(max_length=24, unique=True)
     references_palissy = models.CharField(max_length=20, null=True, blank=True,
                                           help_text="Séparer les codes par des virgules")
     resume = models.TextField(max_length=500, null=True, verbose_name="Resumé", blank=True,
