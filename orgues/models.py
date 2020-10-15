560171e4c38115226c9dc054ed4e4795f305d77d
gwilherm.poullennec <gwilherm.poullennec@gmail.com>
2020-10-15 17:12:48 +0200

    #218 verbose_name

--- orgues/models.py
+++ orgues/models.py
@@ -737,8 +737,8 @@ class Image(models.Model):
     """
     image = models.ImageField(upload_to=chemin_image, help_text="Taille maximale : 2 Mo")
     is_principale = models.BooleanField(default=False, editable=False)
-    legende = models.CharField(max_length=400, null=True, blank=True)
-    credit = models.CharField(max_length=200, null=True, blank=True)
+    legende = models.CharField(max_length=400, null=True, blank=True, verbose_name="Légende")
+    credit = models.CharField(max_length=200, null=True, blank=True, verbose_name="Crédit")
 
     # Champs automatiques
     thumbnail_principale = ProcessedImageField(upload_to=chemin_image,
