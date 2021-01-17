0849a71a790f28095f769fef4f11f2d04fe0af6c
gwilherm.poullennec <gwilherm.poullennec@gmail.com>
2021-01-17 21:36:28 +0100

    [admin] Ajout Accessoire et nettoyage

--- orgues/admin.py
+++ orgues/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Orgue, Clavier, TypeClavier, TypeJeu, Jeu, Fichier, Image, Evenement
+from .models import Orgue, Clavier, TypeClavier, TypeJeu, Fichier, Image, Evenement, Facteur, Accessoire
 
 
 class ClavierInline(admin.StackedInline):
@@ -8,23 +8,16 @@ class ClavierInline(admin.StackedInline):
     extra = 0
 
 
-from .models import Orgue, Clavier, TypeClavier, TypeJeu, Facteur
-
-
-
 @admin.register(Fichier)
 class FichierAdmin(admin.ModelAdmin):
-    list_display = ('pk','file','description','orgue')
+    list_display = ('pk', 'file', 'description', 'orgue')
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('pk','orgue','credit','is_principale')
+    list_display = ('pk', 'orgue', 'credit', 'is_principale')
     list_editable = ('credit',)
 
-@admin.register(Jeu)
-class JeuAdmin(admin.ModelAdmin):
-    list_display = ('type','clavier','commentaire')
 
 @admin.register(Facteur)
 class FacteurAdmin(admin.ModelAdmin):
@@ -33,12 +26,7 @@ class FacteurAdmin(admin.ModelAdmin):
 
 @admin.register(TypeJeu)
 class TypeJeuAdmin(admin.ModelAdmin):
-    list_display = ('nom',)
-
-
-@admin.register(Clavier)
-class ClavierAdmin(admin.ModelAdmin):
-    list_display = ('orgue',)
+    list_display = ('nom', 'hauteur')
 
 
 @admin.register(TypeClavier)
@@ -55,10 +43,16 @@ class EvenementAdmin(admin.ModelAdmin):
     )
     list_editable = ['resume']
 
+
+@admin.register(Accessoire)
+class AccessoireAdmin(admin.ModelAdmin):
+    list_display = ('nom',)
+
+
 @admin.register(Orgue)
 class OrgueAdmin(admin.ModelAdmin):
     fields = ['codification', 'code_insee', 'commune', 'edifice', 'region', 'departement', 'code_departement']
-    list_display = ('codification','designation','commune','edifice','departement','commentaire_admin','updated_by_user','modified_date')
+    list_display = ('codification', 'designation', 'commune', 'edifice', 'departement', 'commentaire_admin', 'updated_by_user', 'modified_date')
     inlines = [ClavierInline]
     list_filter = ('updated_by_user',)
-    search_fields = ('commune','edifice','codification', 'departement')
+    search_fields = ('commune', 'edifice', 'codification', 'departement')
