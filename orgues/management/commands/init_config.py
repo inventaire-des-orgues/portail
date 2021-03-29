25fcb039bb6b5adfc783444315932d240f9b5bb2
CelestinHuet <Celestin.Huet@ensg.eu>
2021-03-29 22:29:49 +0200

    Annulation des modifications sur init_config

--- orgues/management/commands/init_config.py
+++ orgues/management/commands/init_config.py
@@ -19,18 +19,11 @@ class Command(BaseCommand):
             TypeClavier.objects.get_or_create(nom=type_clavier)
 
         for jeu in data["types_jeux"]:
-            liste = TypeJeu.objects.filter(nom=jeu["nom"].strip(), hauteur=jeu["hauteur"].strip())
-            if len(liste) <= 1:
-                TypeJeu.objects.get_or_create(nom=jeu["nom"].strip(), hauteur=jeu["hauteur"].strip())
-            else :
-                print("Doublon : ", jeu, len(liste))
+
+            TypeJeu.objects.get_or_create(nom=jeu["nom"].strip(), hauteur=jeu["hauteur"].strip())
 
         for facteur in data["facteurs"]:
-            liste = Facteur.objects.filter(nom=facteur)
-            if len(liste) <= 1:
-                Facteur.objects.get_or_create(nom=facteur)
-            else :
-                print("Doublon : ", facteur, len(liste))
+            Facteur.objects.get_or_create(nom=facteur)
 
         for accessoire in data["type_accessoire"]:
             Accessoire.objects.get_or_create(nom=accessoire)
