a528e83ece7e8e30d30ac88a544d904701e212a3
Lucas Berbesson <lucas.berbesson@fabdev.fr>
2021-05-10 12:15:33 +0200

    Check if file exists before trying to remove it #498

--- orgues/management/commands/remove_broken_images.py
+++ orgues/management/commands/remove_broken_images.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management.base import BaseCommand
 
 from orgues.models import Image
@@ -10,8 +12,10 @@ class Command(BaseCommand):
         for image in Image.objects.all():
             if not image.image:
                 image.delete()
-                print('Delete non existing image: ',image)
+                print('Delete non existing image: ', image)
+            elif not os.path.exists(image.image.path):
+                print('File does not exist: ', image.image.path)
             elif image.image.size <= 1:
                 image.image.delete()
                 image.delete()
-                print('Delete empty image : ',image)
+                print('Delete empty image : ', image)
