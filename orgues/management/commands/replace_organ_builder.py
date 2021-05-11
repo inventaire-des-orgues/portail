7535b5c7b2f73c8e29235fcf53d4a840470937f3
gwilherm.poullennec <gwilherm.poullennec@gmail.com>
2021-05-11 09:48:29 +0200

    #221

--- orgues/management/commands/replace_organ_builder.py
+++ orgues/management/commands/replace_organ_builder.py
@@ -2,7 +2,6 @@ from orgues.models import Orgue
 from orgues.models import Facteur
 from orgues.models import Evenement
 from django.core.management.base import BaseCommand
-import requests
 
 
 class Command(BaseCommand):
@@ -13,6 +12,9 @@ class Command(BaseCommand):
     - Le nom du facteur par lequel il faut remplacer;
     - optionnel : --delete DELETE : efface de la base de données le facteur à retirer.
     La fonction renvoie un message d'erreur si l'un des facteurs passé en argument n'existe pas dans la base de données ou s'il existe plusieurs fois.
+
+    Ex :
+    manage.py replace_organ_builder --delete "Abbey John II" "Abbey John-Albert"
     """
     help = "Change le facteur d'orgue"
 
@@ -21,33 +23,34 @@ class Command(BaseCommand):
                             help='Nom du facteur à retirer de la base de données.')
         parser.add_argument('name_replace', nargs=1, type=str,
                             help='Nom du facteur qui doit remplacer.')
-        parser.add_argument('--delete', nargs=1, type=str,
+        parser.add_argument('--delete', action='store_true',
                             help="Efface le facteur d'orgue de la base de données")
 
     def handle(self, *args, **options):
         facteur_delete = Facteur.objects.filter(nom=options['name_delete'][0])
         facteur_replace = Facteur.objects.filter(nom=options['name_replace'][0])
         if facteur_delete.count() == 0:
-            print("Le nom du facteur à effacer n'existe pas.")
+            print("ERROR : Le nom du facteur à remplacer n'existe pas.")
         elif facteur_replace.count() == 0:
-            print("Le nom du facteur qui doit remplacer n'existe pas.")
+            print("ERROR : Le nom du facteur qui doit prendre place n'existe pas.")
         elif facteur_delete.count() > 1:
-            print("Deux facteurs à effacer portent le même nom.")
+            print("ERROR : Deux facteurs à effacer portent le même nom.")
         elif facteur_replace.count() > 1:
-            print("Deux facteurs à mettre portent le même nom.")
+            print("ERROR : Deux facteurs à mettre portent le même nom.")
         else:
+            print("INFO : Je cherche à remplacer {} par {}".format(facteur_delete, facteur_replace))
             evenements = Evenement.objects.filter(facteurs=facteur_delete[0])
             for evenement in evenements:
                 evenement.facteurs.remove(facteur_delete[0])
                 evenement.facteurs.add(facteur_replace[0])
-                print("Changement effectué pour l'orgue de {} pour l'événement {}.".format(evenement.orgue, evenement))
+                print("INFO : Changement effectué pour l'orgue de {} pour l'événement {}.".format(evenement.orgue, evenement))
                 evenement.save()
             orgues = Orgue.objects.filter(entretien=facteur_delete[0])
             for orgue in orgues:
                 orgue.entretien.remove(facteur_delete[0])
                 orgue.entretien.add(facteur_replace[0])
-                print("Changement effectué pour l'entretien de l'orgue de {}.".format(orgue))
+                print("INFO : Changement effectué pour l'entretien de l'orgue de {}.".format(orgue))
                 orgue.save()
             if options['delete']:
                 facteur_delete[0].delete()
-                print("Le facteur {} a été retiré de la liste.".format(options['name_delete'][0]))
+                print("INFO : Le facteur {} a été retiré de la liste.".format(options['name_delete'][0]))
