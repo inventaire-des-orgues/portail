e515c449e9efaf3765f0f68448ced3d972a7cf2e
gwilherm.poullennec <gwilherm.poullennec@gmail.com>
2021-05-11 11:38:11 +0200

    PEP8

--- orgues/management/commands/verif_presence_insee.py
+++ orgues/management/commands/verif_presence_insee.py
@@ -4,10 +4,6 @@ from tqdm import tqdm
 import json
 
 
-
-
-
-
 class Command(BaseCommand):
     """
     Récupère les informations des orgues de la base de données qui n'ont pas le champs code_INSEE renseigné.
@@ -18,15 +14,13 @@ class Command(BaseCommand):
         self.liste_insee_manquants = []
 
     def handle(self, *args, **options):
-        nb=0
+        nb = 0
         for orgue in tqdm(Orgue.objects.all()):
             # On recherche les orgues qui n'ont pas de code INSEE renseigné
             if not orgue.code_insee:
-                nb +=1
+                nb += 1
                 self.liste_insee_manquants.append({"codification": orgue.codification, "commune": orgue.commune})
 
         print("Il y a {} orgues dont le code INSEE n'est pas renseigné (voir le détail dans code_INSEE_manquants.json)".format(nb))        
         with open('code_INSEE_manquants.json', 'w') as f:
             json.dump(self.liste_insee_manquants, f)
-
-   
