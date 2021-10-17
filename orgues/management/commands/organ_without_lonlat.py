6ef3d6158df984326d05f61c68c877d84ea9a238
CelestinHuet <celestin.huet@gmail.com>
2021-10-17 09:03:17 +0200

    Commande manage.py pour récupérer la liste des orgues qui n'ont pas de localisation lonlat

---
+++ orgues/management/commands/organ_without_lonlat.py
@@ -0,0 +1,36 @@
+from orgues.models import Orgue
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+import requests
+import json
+import time
+import csv
+from django.db.models import Q
+
+class Command(BaseCommand):
+    """
+    Renvoie sous format csv la liste des orgues qui n'ont pas d'identifiants Open Street Map et qui n'ont pas de localisation.
+    Si l'argument lonlat vaut True, les orgues renvoyés sont ceux qui ne disposent ni de l'identifiant OSM, ni d'une localisation en latitude/longitude
+    Si l'argument lonlat vaut False, les orgues renvoyés sont ceux qui ne disposent pas de l'identifiant OSM
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('lonlat', nargs=1, type=str,
+                            help="Si False, renvoie la liste des orgues dont seul l'identifiant OSM n'est pas renseigné.")
+
+    def handle(self, *args, **options):
+        bdo = Orgue.objects.all()
+        bdo = bdo.filter(Q(osm_id__isnull=True) or Q(osm_type__isnull=True)).distinct()
+        if options['lonlat'][0] == 'True':
+            bdo = bdo.filter(Q(latitude__isnull=True) or Q(longitude__isnull=True)).distinct()
+
+        with open('organ_without_lonlat.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow(["Codification", "Edifice", "Commune", "Code Insee", "Département", "lonlat"])
+            for organ in bdo:
+                liste = [organ.codification, organ.edifice, organ.commune, organ.code_insee, organ.departement]
+                if organ.latitude == None or organ.longitude == None:
+                    liste.append("Non renseigné")
+                else:
+                    liste.append("Renseigné")
+                writer.writerow(liste)
