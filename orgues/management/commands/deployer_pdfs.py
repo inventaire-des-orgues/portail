1325f64faf5629771e32cb335b662fd4544da1b8
g.poullennec <gwilherm.poullennec@gmail.com>
2022-11-11 22:43:18 +0100

    gestion sous-dossiers

--- orgues/management/commands/deployer_pdfs.py
+++ orgues/management/commands/deployer_pdfs.py
@@ -34,26 +34,30 @@ class Command(BaseCommand):
                 tar.extractall(rep_temp_pdf)
                 tar.close()
 
-                # Boucle sur les fichiers du dossier
-                print("[INFO] Parcours des PDF du dossier : {}".format(rep_temp_pdf))
-                for pdf in os.listdir(rep_temp_pdf):
-                    print("[DEBUG] Fichier PDF : {}".format(pdf))
-                    if pdf[:2] != 'FR':
-                        print("[ERROR] nommage fichier {} : le nom ne commence pas par 'FR'".format(pdf))
-                    else:
-                        chemin_pdf = os.path.join(rep_temp_pdf, pdf)
-                        print("[DEBUG] Chemin : {}".format(chemin_pdf))
+                # Boucle sur les dossiers du temporaire
+                for dossier in os.listdir(rep_temp_pdf):
+                    # Boucle sur les fichiers du dossier
+                    print("[INFO] Parcours des PDF du dossier : {}".format(rep_temp_pdf))
+                    for pdf in os.listdir(os.path.join(rep_temp_pdf, dossier)):
+                        print("[DEBUG] Fichier PDF : {}".format(pdf))
+                        if pdf[:2] != 'FR':
+                            print("[ERROR] nommage fichier {} : le nom ne commence pas par 'FR'".format(pdf))
+                        else:
+                            chemin_pdf = os.path.join(rep_temp_pdf, dossier, pdf)
+                            print("[DEBUG] Chemin : {}".format(chemin_pdf))
 
-                        # Changer les permissions
-                        shutil.chown(pdf, user='fabdev', group='www-data')
-                        os.chmod(chemin_pdf, 0o644)
+                            # Changer les permissions
+                            shutil.chown(pdf, user='fabdev', group='www-data')
+                            os.chmod(chemin_pdf, 0o644)
 
-                        dossier_dest = os.path.join(settings.MEDIA_ROOT, pdf[3:5], pdf.rstrip('.pdf'), 'fichiers')
-                        print("[DEBUG] Dossier destination : {}".format(dossier_dest))
-                        if not os.path.exists(dossier_dest):
-                            os.makedirs(dossier_dest)
-                            print("Création dossier : {}".format(dossier_dest))
-                        dest = os.path.join(dossier_dest, pdf)
-                        print("Copie fichier {} vers {}".format(chemin_pdf, dest))
-                        shutil.copyfile(chemin_pdf, dest)
+                            # On dépose le fichier au bon endroit dans l'arborescence
+                            # <racine_projet_django>/media/<code_dep>/<code_orgue>/fichiers/<nomdufichier>
+                            dossier_dest = os.path.join(settings.MEDIA_ROOT, pdf[3:5], pdf.rstrip('.pdf'), 'fichiers')
+                            print("[DEBUG] Dossier destination : {}".format(dossier_dest))
+                            if not os.path.exists(dossier_dest):
+                                os.makedirs(dossier_dest)
+                                print("Création dossier : {}".format(dossier_dest))
+                            dest = os.path.join(dossier_dest, pdf)
+                            print("Copie fichier {} vers {}".format(chemin_pdf, dest))
+                            shutil.copyfile(chemin_pdf, dest)
                 shutil.rmtree(rep_temp_pdf)
