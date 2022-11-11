00037c3294a9de9116ae62634a978c452c6cd0d0
g.poullennec <gwilherm.poullennec@gmail.com>
2022-11-11 22:18:14 +0100

    Gestion des permissions

--- orgues/management/commands/deployer_pdfs.py
+++ orgues/management/commands/deployer_pdfs.py
@@ -21,6 +21,7 @@ class Command(BaseCommand):
         if not os.path.exists(options['path'][0]):
             print("Dossier introuvable !")
         else:
+            # Boucle sur les archives
             for file in os.listdir(options['path'][0]):
                 chemin_tar = os.path.join(options['path'][0], file)
                 tar = tarfile.open(chemin_tar)
@@ -32,17 +33,26 @@ class Command(BaseCommand):
                 tar.extractall(rep_temp_pdf)
                 tar.close()
 
+                # Boucle sur les fichiers du dossier
                 print("Parcours des PDF du dossier : {}".format(rep_temp_pdf))
                 for pdf in os.listdir(rep_temp_pdf):
                     print("[DEBUG] Fichier PDF : {}".format(pdf))
-                    chemin_pdf = os.path.join(rep_temp_pdf, pdf)
-                    print("[DEBUG] Chemin : {}".format(chemin_pdf))
-                    dossier_dest = os.path.join(settings.MEDIA_ROOT, pdf[3:5], pdf.rstrip('.pdf'), 'fichiers')
-                    print("[DEBUG] Dossier destination : {}".format(dossier_dest))
-                    if not os.path.exists(dossier_dest):
-                        os.makedirs(dossier_dest)
-                        print("Création dossier : {}".format(dossier_dest))
-                    dest = os.path.join(dossier_dest, pdf)
-                    print("Copie fichier {} vers {}".format(chemin_pdf, dest))
-                    shutil.copyfile(chemin_pdf, dest)
+                    if pdf[:2] != 'FR':
+                        print("[ERROR] nommage fichier {} : le nom ne commence pas par FR)".format(pdf))
+                    else:
+                        chemin_pdf = os.path.join(rep_temp_pdf, pdf)
+                        print("[DEBUG] Chemin : {}".format(chemin_pdf))
+
+                        # Changer les permissions
+                        shutil.chown(pdf, user='fabdev', group='www-data')
+                        os.chmod(chemin_pdf, 0o644)
+
+                        dossier_dest = os.path.join(settings.MEDIA_ROOT, pdf[3:5], pdf.rstrip('.pdf'), 'fichiers')
+                        print("[DEBUG] Dossier destination : {}".format(dossier_dest))
+                        if not os.path.exists(dossier_dest):
+                            os.makedirs(dossier_dest)
+                            print("Création dossier : {}".format(dossier_dest))
+                        dest = os.path.join(dossier_dest, pdf)
+                        print("Copie fichier {} vers {}".format(chemin_pdf, dest))
+                        shutil.copyfile(chemin_pdf, dest)
                 shutil.rmtree(rep_temp_pdf)
