08801e2c7a446d9cb86c23008a0bfa0066eff75c
CelestinHuet <celestin.huet@gmail.com>
2024-09-14 10:24:00 +0200

    Remplacement de print par logging

--- orgues/management/commands/facteur_to_manufactures.py
+++ orgues/management/commands/facteur_to_manufactures.py
@@ -3,7 +3,10 @@ from orgues.models import Facteur, Manufacture
 from orgues.models import Evenement
 from django.core.management.base import BaseCommand
 import os
+import logging
 
+
+logger = logging.getLogger("facteur_to_manufacture")
 
 class Command(BaseCommand):
     """
@@ -30,15 +33,15 @@ class Command(BaseCommand):
             return "Fichier introuvable".format(options['codestable'][0])
         else:
             with open(options['codestable'][0], "r", encoding="utf-8") as f:
-                print("Début traitement d'une liste de codes à remplacer.")
+                logger.info("Début traitement d'une liste de codes à remplacer.")
                 couples_facteurs = [ligne.rstrip('\n').split(';') for ligne in f.readlines()]
                 for couple_facteur in couples_facteurs:
                     facteur_avant = couple_facteur[0]
                     facteur_delete = Facteur.objects.filter(nom=facteur_avant)
                     if facteur_delete.count() == 0:
-                        print("ERROR : Le nom du facteur à remplacer n'existe pas : {}".format(facteur_avant))
+                        logger.error("Le nom du facteur à remplacer n'existe pas : {}".format(facteur_avant))
                     elif facteur_delete.count() > 1:
-                        print("ERROR : Deux facteurs à effacer portent le même nom : {}".format(facteur_avant))
+                        logger.error("Deux facteurs à effacer portent le même nom : {}".format(facteur_avant))
                     else:
                         liste_manufacture_apres = []
                         erreur = False
@@ -48,26 +51,26 @@ class Command(BaseCommand):
                             liste_manufacture_apres.append(manufacture_replace)
                             if manufacture_replace.count() == 0:
                                 erreur = True
-                                print("ERROR : Le nom de la manufacture qui doit prendre place n'existe pas : {}".format(manufacture_apres))
+                                logger.error("Le nom de la manufacture qui doit prendre place n'existe pas : {}".format(manufacture_apres))
                             elif manufacture_replace.count() > 1:
                                 erreur = True
-                                print("ERROR : Deux manufactures à mettre portent le même nom : {}".format(manufacture_apres))
+                                logger.error("Deux manufactures à mettre portent le même nom : {}".format(manufacture_apres))
                         if not erreur:
-                            print("INFO : Je cherche à remplacer {} par {}".format(facteur_delete, liste_manufacture_apres))
+                            logger.info("Je cherche à remplacer {} par {}".format(facteur_delete, liste_manufacture_apres))
                             evenements = Evenement.objects.filter(facteurs=facteur_delete[0])
                             for evenement in evenements:
                                 for manufacture_replace in liste_manufacture_apres:
                                     evenement.manufactures.add(manufacture_replace[0])
                                 evenement.facteurs.remove(facteur_delete[0])
-                                print("INFO : Changement effectué pour l'orgue de {} pour l'événement {}.".format(evenement.orgue, evenement))
+                                logger.info("Changement effectué pour l'orgue de {} pour l'événement {}.".format(evenement.orgue, evenement))
                                 evenement.save()
                             orgues = Orgue.objects.filter(entretien=facteur_delete[0])
                             for orgue in orgues:
                                 for manufacture_replace in liste_manufacture_apres:
                                     orgue.entretienManufacture.add(manufacture_replace[0])
                                 orgue.entretien.remove(facteur_delete[0])
-                                print("INFO : Changement effectué pour l'entretien de l'orgue de {}.".format(orgue))
+                                logger.info("Changement effectué pour l'entretien de l'orgue de {}.".format(orgue))
                                 orgue.save()
                             if options['delete']:
-                                print("INFO : Le facteur {} a été retiré de la liste.".format(facteur_delete))
+                                logger.info("Le facteur {} a été retiré de la liste.".format(facteur_delete))
                                 facteur_delete[0].delete()
